cb896cb5ade7c65687649d8b3c032babe18d1bce
Aaron Roberson <aaronaroberson@gmail.com>
2015-10-03 15:01:13 -0700

    chore(meteor): add package for meteor

---
+++ package.js
@@ -0,0 +1,24 @@
+// Package metadata file for Meteor.js
+var packageName = 'aaronroberson:angular-motion';
+var where = 'client';
+var version = '0.4.3';
+var summary = 'AngularMotion - Fancy CSS3 animations for AngularJS 1.2+';
+var gitLink = 'https://github.com/aaronroberson/angular-motion';
+var documentationFile = 'README.md';
+
+// Meta-data
+Package.describe({
+  name: packageName,
+  version: version,
+  summary: summary,
+  git: gitLink,
+  documentation: documentationFile
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']);
+
+  api.use('angular:angular@1.3.0', where)
+
+  api.addFiles('dist/angular-motion.css', where);
+}); 
